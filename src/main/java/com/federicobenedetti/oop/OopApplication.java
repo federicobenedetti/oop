2a7f53af62ec988f898d10fd92dbd53a321405f4
Federico Benedetti <fede.benedetti9@gmail.com>
2020-02-17 18:04:08 +0100

    Add initial implementation of a DownloadManager

--- src/main/java/com/federicobenedetti/oop/OopApplication.java
+++ src/main/java/com/federicobenedetti/oop/OopApplication.java
@@ -6,13 +6,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class OopApplication {
 
-	private String DATASET = "https://data.europa.eu/euodp/data/api/3/action/package_show?id=1KNEJIFSG7jHcPFDk61Vcg";
+	private static String DATASET = "https://data.europa.eu/euodp/data/api/3/action/package_show?id=1KNEJIFSG7jHcPFDk61Vcg";
+	private static DataSet _dataset = new DataSet();
+	private static DatasetDownloadManager _ddm = new DatasetDownloadManager(DATASET);
 
 	public static void main(String[] args) {
 		// First let's start Spring
 		SpringApplication.run(OopApplication.class, args);
 
-		// Now let's
+		// Now let's download
+		_ddm.DownloadDataset();
 	}
 
 }
