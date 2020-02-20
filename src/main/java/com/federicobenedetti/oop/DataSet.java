85cdb477c26039de3213af3f9209312951c4b21e
Federico Benedetti <fede.benedetti9@gmail.com>
2020-02-20 16:18:13 +0100

    Let's use HashMap instead
    Signed-off-by: Federico Benedetti <fede.benedetti9@gmail.com>

--- src/main/java/com/federicobenedetti/oop/DataSet.java
+++ src/main/java/com/federicobenedetti/oop/DataSet.java
@@ -1,25 +1,10 @@
 package com.federicobenedetti.oop;
 
+import java.util.HashMap;
+
 public class DataSet {
     private String BmiSexAgeQuantileTimeGeo;
-    private double BE;
-    private double BG;
-    private double CZ;
-    private double DE;
-    private double EE;
-    private double EL;
-    private double ES;
-    private double FR;
-    private double CY;
-    private double LV;
-    private double HU;
-    private double MT;
-    private double AT;
-    private double PL;
-    private double RO;
-    private double SI;
-    private double SK;
-    private double TR;
+    private HashMap<String, Double> DataSetValues = new HashMap<String, Double>();
 
     public DataSet() {
     }
@@ -27,26 +12,21 @@ public class DataSet {
     @Override
     public String toString() {
         return "DataSet{" +
-                "BmiSexAgeQuantileTimeGeo = '" + BmiSexAgeQuantileTimeGeo + '\'' +
-                ", BE = " + BE +
-                ", BG = " + BG +
-                ", CZ = " + CZ +
-                ", DE = " + DE +
-                ", EE = " + EE +
-                ", EL = " + EL +
-                ", ES = " + ES +
-                ", FR = " + FR +
-                ", CY = " + CY +
-                ", LV = " + LV +
-                ", HU = " + HU +
-                ", MT = " + MT +
-                ", AT = " + AT +
-                ", PL = " + PL +
-                ", RO = " + RO +
-                ", SI = " + SI +
-                ", SK = " + SK +
-                ", TR = " + TR +
+                "BmiSexAgeQuantileTimeGeo='" + BmiSexAgeQuantileTimeGeo + '\'' +
+                ", DataSetValues=" + DataSetValues +
                 '}';
+    }
+
+    public void SetHashMapKeyValuePair(String key, String value) {
+        double tempValue;
+        if (value.isEmpty()) {
+            tempValue = 0;
+        } else {
+            tempValue = Double.parseDouble(value);
+        }
+
+        DataSetValues.put(key, tempValue);
+        return;
     }
 
     public String getBmiSexAgeQuantileTimeGeo() {
@@ -55,153 +35,5 @@ public class DataSet {
 
     public void setBmiSexAgeQuantileTimeGeo(String bmiSexAgeQuantileTimeGeo) {
         BmiSexAgeQuantileTimeGeo = bmiSexAgeQuantileTimeGeo;
-    }
-
-    private double StringToBoolean(String number) {
-        return Double.parseDouble(number);
-    }
-
-    public double getBE() {
-        return BE;
-    }
-
-    public void setBE(String BE) {
-        this.BE = StringToBoolean(BE);
-    }
-
-    public double getBG() {
-        return BG;
-    }
-
-    public void setBG(String BG) {
-        this.BG = StringToBoolean(BG);
-    }
-
-    public double getCZ() {
-        return CZ;
-    }
-
-    public void setCZ(String CZ) {
-        this.CZ = StringToBoolean(CZ);
-    }
-
-    public double getDE() {
-        return DE;
-    }
-
-    public void setDE(String DE) {
-        this.DE = StringToBoolean(DE);
-    }
-
-    public double getEE() {
-        return EE;
-    }
-
-    public void setEE(String EE) {
-        this.EE = StringToBoolean(EE);
-    }
-
-    public double getEL() {
-        return EL;
-    }
-
-    public void setEL(String EL) {
-        this.EL = StringToBoolean(EL);
-    }
-
-    public double getES() {
-        return ES;
-    }
-
-    public void setES(String ES) {
-        this.ES = StringToBoolean(ES);
-    }
-
-    public double getFR() {
-        return FR;
-    }
-
-    public void setFR(String FR) {
-        this.FR = StringToBoolean(FR);
-    }
-
-    public double getCY() {
-        return CY;
-    }
-
-    public void setCY(String CY) {
-        this.CY = StringToBoolean(CY);
-    }
-
-    public double getLV() {
-        return LV;
-    }
-
-    public void setLV(String LV) {
-        this.LV = StringToBoolean(LV);
-    }
-
-    public double getHU() {
-        return HU;
-    }
-
-    public void setHU(String HU) {
-        this.HU = StringToBoolean(HU);
-    }
-
-    public double getMT() {
-        return MT;
-    }
-
-    public void setMT(String MT) {
-        this.MT = StringToBoolean(MT);
-    }
-
-    public double getAT() {
-        return AT;
-    }
-
-    public void setAT(String AT) {
-        this.AT = StringToBoolean(AT);
-    }
-
-    public double getPL() {
-        return PL;
-    }
-
-    public void setPL(String PL) {
-        this.PL = StringToBoolean(PL);
-    }
-
-    public double getRO() {
-        return RO;
-    }
-
-    public void setRO(String RO) {
-        this.RO = StringToBoolean(RO);
-    }
-
-    public double getSI() {
-        return SI;
-    }
-
-    public void setSI(String SI) {
-        this.SI = StringToBoolean(SI);
-    }
-
-    public double getSK() {
-        return SK;
-    }
-
-    public void setSK(String SK) {
-        this.SK = StringToBoolean(SK);
-    }
-
-    public double getTR() {
-        return TR;
-    }
-
-    public void setTR(String TR) {
-        this.TR = StringToBoolean(TR);
     }
 }
--- src/main/java/com/federicobenedetti/oop/DataSetParser.java
+++ src/main/java/com/federicobenedetti/oop/DataSetParser.java
@@ -20,77 +20,22 @@ public class DataSetParser {
         String row;
         DataSet ds;
 
-        csvReader.readLine();   // Skipping header
+        String[] header = csvReader.readLine().split("\t");
+
         while ((row = csvReader.readLine()) != null) {
             String[] data = row.split("\t");
-
             ds = new DataSet();
 
             // Last line is dirty
             for (int i = 0; i < data.length - 1; i++) {
                 String val = SanitizeValue(data[i], i);
 
-                switch (i) {
-                    case 0:
-                        ds.setBmiSexAgeQuantileTimeGeo(val);
-                        break;
-                    case 1:
-                        ds.setBE(val);
-                        break;
-                    case 2:
-                        ds.setBG(val);
-                        break;
-                    case 3:
-                        ds.setCZ(val);
-                        break;
-                    case 4:
-                        ds.setDE(val);
-                        break;
-                    case 5:
-                        ds.setEE(val);
-                        break;
-                    case 6:
-                        ds.setEL(val);
-                        break;
-                    case 7:
-                        ds.setES(val);
-                        break;
-                    case 8:
-                        ds.setFR(val);
-                        break;
-                    case 9:
-                        ds.setCY(val);
-                        break;
-                    case 10:
-                        ds.setLV(val);
-                        break;
-                    case 11:
-                        ds.setHU(val);
-                        break;
-                    case 12:
-                        ds.setMT(val);
-                        break;
-                    case 13:
-                        ds.setAT(val);
-                        break;
-                    case 14:
-                        ds.setPL(val);
-                        break;
-                    case 15:
-                        ds.setRO(val);
-                        break;
-                    case 16:
-                        ds.setSI(val);
-                        break;
-                    case 17:
-                        ds.setSK(val);
-                        break;
-                    case 18:
-                        ds.setTR(val);
-                        break;
-                    default:
-                        break;
+                if (i == 0) {
+                    ds.setBmiSexAgeQuantileTimeGeo(val);
+                } else {
+                    ds.SetHashMapKeyValuePair(header[i], val);
                 }
+
             }
             System.out.println("DataSet: " + ds.toString());
             this._dataSet.add(ds);
